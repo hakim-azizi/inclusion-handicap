5ad37b5b40d641f0088cc34426a70174d85a5327
hakim-azizi <160917355+hakim-azizi@users.noreply.github.com>
2024-07-04 12:13:34 +0200

    add router and home page, and style css

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "../style/footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; handisuite</p>
+      <p>All rights reserved 03:07:2024 12:12:2024</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,58 @@
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import "../style/header.css";
+
+function Header() {
+  const [open, setOpen] = useState(0);
+  const [menuBurger, setMenuBurger] = useState("images/menu-burger.png");
+  const openMenu = () => {
+    if (open === 0) {
+      setOpen(1);
+      document.querySelector("#vu-menu").style.display = "block";
+      setMenuBurger("images/menu-burger1.png");
+    } else {
+      setOpen(0);
+      document.querySelector("#vu-menu").style.display = "none";
+      setMenuBurger("images/menu-burger.png");
+    }
+    // document.querySelector("#menu-burger").innerHTML =
+    // `<img src=${menuBurger} alt="ouverture du menu">`;
+  };
+  return (
+    <header className="nav-bar">
+      <nav>
+        <figure>
+          <img src="images/logo.jpg" alt="logo" />
+        </figure>
+        <ul id="vu-menu">
+          <li>
+            <a href="./">acceuil</a>
+          </li>
+          <li>
+            <NavLink to="page-generique.html">lien 1</NavLink>
+          </li>
+          <li>
+            <NavLink to="page-generique.html">lien 1</NavLink>
+          </li>
+          <li>
+            <NavLink to="page-generique.html">lien 1</NavLink>
+          </li>
+          <li>
+            <NavLink to="page-generique.html">lien 1</NavLink>
+          </li>
+          <li>
+            <NavLink to="page-generique.html">lien 1</NavLink>
+          </li>
+        </ul>
+        <figure id="menu-burger">
+          <button type="button" onClick={openMenu}>
+            {" "}
+            <img src={menuBurger} alt="ouverture du menu" />
+          </button>
+        </figure>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
